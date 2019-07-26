346c8dd868fff8205e571490a8de543efabb3dc0
Uther <625124155@qq.com>
2019-07-26 10:43:14 +0800

    test 2

--- node_exporter/run.py
+++ node_exporter/run.py
@@ -8,32 +8,30 @@ HOST = '0.0.0.0'
 socket_port = 5858
 api_port = 9100
 
-run_web(HOST, api_port)
+run_socket = Process(target=run_socket, args=(socket_port,))
+run_api = Process(target=run_web, args=(api_port,))
 
-# run_socket = Process(target=run_socket, args=(socket_port,))
-# run_api = Process(target=run_web, args=(api_port,))
+if __name__ == '__main__':
+    while True:
+        try:
+            run_socket.start()
+            logger.info('starting socket server on {}:{}.'.format(HOST, str(socket_port)))
 
-# if __name__ == '__main__':
-#     while True:
-#         try:
-#             run_socket.start()
-#             logger.info('starting socket server on {}:{}.'.format(HOST, str(socket_port)))
-#
-#             run_api.start()
-#             logger.info('starting api server on {}:{}.'.format(HOST, str(api_port)))
-#
-#         except Exception as e:
-#             logger.exception('stopped server on {}:{}'.format(HOST, str(socket_port)))
-#             logger.exception(e)
-#
-#         try:
-#             run_api.start()
-#             logger.info('starting api server on {}:{}.'.format(HOST, str(api_port)))
-#         except Exception as e:
-#             logger.exception('stopped server on {}:{}'.format(HOST, str(api_port)))
-#             logger.exception(e)
-#
-#         time.sleep(3)
+            run_api.start()
+            logger.info('starting api server on {}:{}.'.format(HOST, str(api_port)))
+
+        except Exception as e:
+            logger.exception('stopped server on {}:{}'.format(HOST, str(socket_port)))
+            logger.exception(e)
+
+        try:
+            run_api.start()
+            logger.info('starting api server on {}:{}.'.format(HOST, str(api_port)))
+        except Exception as e:
+            logger.exception('stopped server on {}:{}'.format(HOST, str(api_port)))
+            logger.exception(e)
+
+        time.sleep(3)
 
 
 
