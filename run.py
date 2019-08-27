55bd3ff8147a0cf6fa9249ce8edfdfaf8aad199f
Uther <625124155@qq.com>
2019-08-27 17:10:48 +0800

    数据持久化

--- run.py
+++ run.py
@@ -2,40 +2,24 @@
 import os
 
 
-
-
-
-
-
-
-
-
-
-
-
-def run(*cmds):
+def run(cmds):
     for cmd in cmds:
         os.system(cmd)
 
 print("请使用 root 用户执行")
-c1 =  'groupadd -g 472 grafana'
-c2 =  'useradd -u 472 -g grafana grafana'
-c3 = 'chown  -R grafana.grafana ./grafana'
-c4 = 'docker stack deploy --compose-file=docker-compose.yml quatek'
-
 
 cmd = [
     'groupadd -g 472 grafana',
     'useradd -u 472 -g grafana grafana',
-    'chown  -R grafana.grafana ./grafana',
+    'chown  -R grafana.grafana ./data/grafana',
 
     
     'groupadd -g 65534 nobody',# 还有问题  需要调整
     'useradd -u 65534 -g nobody',
-    'chown  -R nobody ./prometheus/data/prometheus',
+    'chown  -R nobody ./data/prometheus',
     
     'docker stack deploy --compose-file=docker-compose.yml quatek',
     ]
 
 
-run(c1, c2, c3, c4)
+run(cmd)
